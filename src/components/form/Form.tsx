a8a7627e9aa76a2dc5143ad202420f15bc7e94b3
Ulyana Sanina <sanina.ulyana111@gmail.com>
2023-10-23 15:23:00 +0400

    add Keyboard to Form as compound components

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./Form.css";
+import Keyboard from "../keyboard";
+
 export const Form = () => {
   const [phoneNumber, setPhoneNumber] = useState("+7(___)___-__-__");
 
@@ -9,6 +11,19 @@ export const Form = () => {
         <h1 className="form_title">Введите ваш номер мобильного телефона</h1>
         <p className="form_number">{phoneNumber}</p>
         <p>и с Вами свяжется наш менеджер для дальнейшей консультации</p>
+        <Keyboard setPhoneNumber={setPhoneNumber} phoneNumber={phoneNumber}>
+          <Keyboard.Number number={1} />
+          <Keyboard.Number number={2} />
+          <Keyboard.Number number={3} />
+          <Keyboard.Number number={4} />
+          <Keyboard.Number number={5} />
+          <Keyboard.Number number={6} />
+          <Keyboard.Number number={7} />
+          <Keyboard.Number number={8} />
+          <Keyboard.Number number={9} />
+          <Keyboard.Clear />
+          <Keyboard.Number number={0} />
+        </Keyboard>
         <label htmlFor="data" className="check">
           <input
             type="checkbox"
